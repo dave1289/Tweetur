503554eea38966ca414dc13a0e41b3fdfa375975
dave1289 <104801768+dave1289@users.noreply.github.com>
2022-07-12 15:48:48 -0400

    Update key.js
    learned delegation and updated file.

--- key.js
+++ key.js
@@ -7,20 +7,27 @@ document.querySelector('input').addEventListener('keydown', function(e){
     console.log('KEY PRESSED', e.key);
 })
 
-for (let btn of removeFriend) {
-    btn.addEventListener('click', function(e){
+// for (let btn of removeFriend) {
+//     btn.addEventListener('click', function(e){
+//         e.target.parentElement.remove();
+//     })
+// }
+
+// learned delegation, reduce code length/complexity.
+friendsList.addEventListener('click', function(e){
+    if (e.target.tagName === 'BUTTON') {
         e.target.parentElement.remove();
-    })
-}
+    };
+})
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log(input.value)
     const newLi = document.createElement('li');
     const removeBtn = document.createElement('button')
-    removeBtn.addEventListener('click', function(e){
-        e.target.parentElement.remove();
-    })
+//     removeBtn.addEventListener('click', function(e){
+//         e.target.parentElement.remove();
+//     })
     removeBtn.textContent = 'Unfriend'
     removeBtn.classList.add('unfriend')
     newLi.textContent = input.value;
