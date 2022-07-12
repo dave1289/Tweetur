1bf9c3281b5b10d6516bbcbfb3918d23238eaaad
dave1289 <104801768+dave1289@users.noreply.github.com>
2022-07-12 15:39:12 -0400

    Add files via upload

---
+++ key.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#add-friend')
+const input = document.querySelector('#first-name')
+const friendsList = document.querySelector('#friends-list')
+const removeFriend = document.querySelectorAll('li button')
+
+document.querySelector('input').addEventListener('keydown', function(e){
+    console.log('KEY PRESSED', e.key);
+})
+
+for (let btn of removeFriend) {
+    btn.addEventListener('click', function(e){
+        e.target.parentElement.remove();
+    })
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(input.value)
+    const newLi = document.createElement('li');
+    const removeBtn = document.createElement('button')
+    removeBtn.addEventListener('click', function(e){
+        e.target.parentElement.remove();
+    })
+    removeBtn.textContent = 'Unfriend'
+    removeBtn.classList.add('unfriend')
+    newLi.textContent = input.value;
+    friendsList.appendChild(newLi)
+    newLi.appendChild(removeBtn)
+    input.value = ''
+}) 
+
